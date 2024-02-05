daf1cf3dda08ac8865b562d69df2945cbb9bdc15
DinossauroBebado <guipsilva32@gmail.com>
2024-02-05 20:37:10 -0300

    task merge micro_ros with ps4 controller with joy msg

--- src/MAIN/main.cpp
+++ src/MAIN/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
-// #include <MAIN/ps4.h>
+#include <MAIN/ps4.h>
 #include <MAIN/micro_ros.h>
 #include "rampa.h"
 // test
-
+#define LED_BUILDIN 2
 bool connected = false;
 bool emergency_break = false;
 int battery_level = 0; 
 bool circle = false;
 bool triangle = false; 
-int cmd_linear = 0 ;
-int cmd_angular = 0 ;
+// int cmd_linear = 0 ;
+// int cmd_angular = 0 ;
 
 
 
@@ -19,26 +19,26 @@ void setup() {
   pinMode(LED_BUILDIN,OUTPUT);
   digitalWrite(LED_BUILDIN, LOW);
   ros_init();
- // PS4setup();
+  PS4setup();
 }
 
 
 void loop() {
- // connected = ps4Connected();
+ connected = ps4Connected();
 
-  // if(connected){
-  //   digitalWrite(LED_BUILDIN, HIGH);
-  //   cmd_linear = ps4Linear();
-  //   if((cmd_linear > last_cmd_linear && last_cmd_linear >= 0) || (last_cmd_linear > cmd_linear && last_cmd_linear <= 0))
-  //     cmd_linear = rampa(ps4Linear(), 80, LINEAR);
-  //   cmd_angular = ps4Angular();
-  //   last_cmd_linear = cmd_linear;
+  if(connected){
+    digitalWrite(LED_BUILDIN, HIGH);
+    cmd_linear = ps4Linear();
+    if((cmd_linear > last_cmd_linear && last_cmd_linear >= 0) || (last_cmd_linear > cmd_linear && last_cmd_linear <= 0))
+      cmd_linear = rampa(ps4Linear(), 80, LINEAR);
+    cmd_angular = ps4Angular();
+    last_cmd_linear = cmd_linear;
 
-  //   emergency_break = ps4X();
-  //   circle = ps4Circle();
-  //   triangle = ps4Triangle();
+    emergency_break = ps4X();
+    circle = ps4Circle();
+    triangle = ps4Triangle();
 
-  //   battery_level = battery();
+    battery_level = battery();
     
     // if(battery(30)){
     //     //ADD BLINK 
@@ -53,10 +53,11 @@ void loop() {
     // Serial.println("");
 
 
-  // }else{
-    // digitalWrite(LED_BUILDIN, LOW);
-  // }
+  }else{
+    digitalWrite(LED_BUILDIN, LOW);
+  }
   ros_loop(cmd_linear,cmd_angular,emergency_break, triangle,circle,battery_level,connected);
-  
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  // ros_loop(0,0,0, 0,0,0,0);
+
+ 
 }
--- src/MAIN/micro_ros.h
+++ src/MAIN/micro_ros.h
@@ -1,33 +1,26 @@
-#include <Arduino.h>
-
 #include <micro_ros_arduino.h>
-
-#include <stdio.h>
-#include <rcl/rcl.h>
-#include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-#include "config.h"
 #include <sensor_msgs/msg/joy.h>
 #include <Arduino.h>
 
 
+#define LED_PIN 2
+#define JOY_MSG_SIZE 4  // Number of elements in the axes array
+#define BOTTON_MSG_SIZE 4
+
 
 //------------------SUBS--------------------
 
-#define joy_topic "joy/controler/ps4"
-#define battery_level_topic "joy/controler/ps4/battery"
-#define controler_connected_topic "joy/controler/connected"
+#define joy_topic "joy/controller/ps4"
+#define battery_level_topic "joy/controller/ps4/battery"
+#define controler_connected_topic "joy/controller/connected"
 
 
 rclc_executor_t executor;
 rcl_node_t node;
 rcl_publisher_t joy_publisher;
 sensor_msgs__msg__Joy joy_msg;
-
-#define LED_PIN 2
-#define JOY_MSG_SIZE 4  // Number of elements in the axes array
-#define BOTTON_MSG_SIZE 4
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
@@ -57,7 +50,7 @@ void ros_init(){
         &joy_publisher,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Joy),
-        "joy"));
+        joy_topic));
 
     // Initialize the Joy message
     // joy_msg.header.frame_id.data = "joy_frame";
@@ -84,22 +77,24 @@ void ros_init(){
 void ros_loop( int cmd_vel_linear, int  cmd_vel_angular, int emergency_break,int triangle,int circle, int battery_level, bool connected){
   
     // JOYSTICKS 
-    joy_msg.axes.data[0] = 0; //cmd_vel_linear ; //Y left stick 
+    joy_msg.axes.data[0] = cmd_vel_linear; //cmd_vel_linear ; //Y left stick 
     joy_msg.axes.data[1] = 0 ; // //X left stick 
-    joy_msg.axes.data[2] = 0 ;//cmd_vel_angular; //Y right stick 
+    joy_msg.axes.data[2] = cmd_vel_angular ;//cmd_vel_angular; //Y right stick 
     joy_msg.axes.data[3] = 0 ;//X right stick 
 
     // BOTTONS  
 
-    joy_msg.buttons.data[0] = 0 ; //emergency_break; // X 
-    joy_msg.buttons.data[1] = 0 ; //circle; //  O 
-    joy_msg.buttons.data[2] = 0 ;//triangle;// triangulo 
-    joy_msg.buttons.data[3] = 0 ;
-    // for (size_t j = 3; j < BOTTON_MSG_SIZE; ++j) {
+    joy_msg.buttons.data[0] = emergency_break ; //emergency_break; // X 
+    joy_msg.buttons.data[1] = circle ; //circle; //  O 
+    joy_msg.buttons.data[2] = triangle ;//triangle;// triangulo 
+    joy_msg.buttons.data[3] = 0 ; // no button
+    for (size_t j = 3; j < BOTTON_MSG_SIZE; ++j) {
         
-    //     joy_msg.buttons.data[j] = 0;  
-    // }
+        joy_msg.buttons.data[j] = 0;    
+    }
 
     RCCHECK(rcl_publish(&joy_publisher, &joy_msg, NULL));
+    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+    delay(100);
     
 }
