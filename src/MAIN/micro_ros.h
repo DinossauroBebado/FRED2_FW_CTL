033049fbcba4a04db42bb84cb4af545cd9c9b86e
DinossauroBebado <guipsilva32@gmail.com>
2024-02-05 21:59:05 -0300

    task: complete first stable version off the controller firmaware

--- src/MAIN/micro_ros.h
+++ src/MAIN/micro_ros.h
@@ -1,7 +1,10 @@
 #include <micro_ros_arduino.h>
 #include <rclc/rclc.h>
+#include <rcl/rcl.h>
 #include <rclc/executor.h>
 #include <sensor_msgs/msg/joy.h>
+#include <std_msgs/msg/int16.h>
+#include <std_msgs/msg/bool.h>
 #include <Arduino.h>
 
 
@@ -10,7 +13,7 @@
 #define BOTTON_MSG_SIZE 4
 
 
-//------------------SUBS--------------------
+//------------------PUBS--------------------
 
 #define joy_topic "joy/controller/ps4"
 #define battery_level_topic "joy/controller/ps4/battery"
@@ -18,9 +21,20 @@
 
 
 rclc_executor_t executor;
+rcl_allocator_t allocator;
+rclc_support_t support;
 rcl_node_t node;
+
 rcl_publisher_t joy_publisher;
 sensor_msgs__msg__Joy joy_msg;
+
+rcl_publisher_t bat_publisher ; 
+std_msgs__msg__Int16 bat_msg;
+
+rcl_publisher_t connect_publisher ; 
+std_msgs__msg__Bool connect_msg;
+
+
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
@@ -41,9 +55,26 @@ void ros_init(){
 
     // Initialize ROS
     rcl_allocator_t allocator = rcl_get_default_allocator();
-    rclc_support_t support;
+  
     RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
     RCCHECK(rclc_node_init_default(&node, "joy_node", "", &support));
+
+
+    RCCHECK(rclc_publisher_init_default(
+      &bat_publisher,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+      battery_level_topic));
+
+    bat_msg.data = 0 ;
+
+    RCCHECK(rclc_publisher_init_default(
+      &connect_publisher,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool),
+      controler_connected_topic));
+
+    connect_msg.data = 0 ;
 
     // Create Joy publisher
     RCCHECK(rclc_publisher_init_default(
@@ -76,6 +107,10 @@ void ros_init(){
 
 void ros_loop( int cmd_vel_linear, int  cmd_vel_angular, int emergency_break,int triangle,int circle, int battery_level, bool connected){
   
+    bat_msg.data = battery_level*10;
+
+    connect_msg.data = connected;
+
     // JOYSTICKS 
     joy_msg.axes.data[0] = cmd_vel_linear; //cmd_vel_linear ; //Y left stick 
     joy_msg.axes.data[1] = 0 ; // //X left stick 
@@ -93,7 +128,12 @@ void ros_loop( int cmd_vel_linear, int  cmd_vel_angular, int emergency_break,int
         joy_msg.buttons.data[j] = 0;    
     }
 
+    RCCHECK(rcl_publish(&connect_publisher, &connect_msg, NULL));
+
     RCCHECK(rcl_publish(&joy_publisher, &joy_msg, NULL));
+
+    RCCHECK(rcl_publish(&bat_publisher, &bat_msg, NULL));
+
     rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
     delay(100);
     
